21f49eb60f229c831f8b3de9b5a4a2b18c13829e
Iaroslav33357999 <Iaroslav33357999@gmail.com>
2026-01-17 22:17:08 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 4000;
+
+// Раздаем статические файлы из папки public
+// Это надежнее, чем просто "templates", так принято в продакшене
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`-----------------------------------------------`);
+    console.log(`Сервер запущен! Локально: http://localhost:${PORT}`);
+    console.log(`Игра готова к работе.`);
+    console.log(`-----------------------------------------------`);
+});
